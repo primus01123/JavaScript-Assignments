c8513b49352c852216679bad81551d8ea4b38aa5
primus01123 <chimmeremzel@gmail.com>
2025-06-19 12:50:09 +0100

    for_loop_practice.js

---
+++ for_loop_practice.js
@@ -0,0 +1,18 @@
+// File: for_loop_practice.js
+// Task: Use for loops to print numbers.
+// Requirements:
+// Use console.log() for each number.
+// Include comments explaining each loop.
+
+// For loop that prints all even numbers from 2 to 10
+// Start at 2, increment by 2 each time to get even numbers
+for (let i = 2; i <= 10; i += 2) {
+  console.log(i);
+}
+
+// For loop that prints numbers from 10 to 1 in descending order
+// Start at 10, decrement by 1 until we reach 1
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
